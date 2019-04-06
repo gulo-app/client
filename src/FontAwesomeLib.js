c47f2f1392a5124efb24d2a8975f694693ae9f7d
flomermer <flom.tomer@gmail.com>
2019-04-07 01:16:02 +0300

    stable skeleton SideMenu

--- src/FontAwesomeLib.js
+++ src/FontAwesomeLib.js
@@ -1,12 +1,14 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faSearch, faEllipsisV, faPlus, faMinus, faUndo } from '@fortawesome/free-solid-svg-icons';
 import { faHome, faDesktop, faHotel, faArchive, faArrowLeft, faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faClipboardList, faComment, faPowerOff} from '@fortawesome/free-solid-svg-icons';
 
-import { faFile } from '@fortawesome/free-regular-svg-icons';
+import { faFile, faComment as farComment} from '@fortawesome/free-regular-svg-icons';
 
 library.add(
   faBars, faSearch, faHome, faEllipsisV, faPlus, faMinus, faUndo,
-  faDesktop, faHotel, faArchive, faFile, faArrowLeft, faTrash  
+  faDesktop, faHotel, faArchive, faFile, faArrowLeft, faTrash,
+  faClipboardList, faComment, farComment, faPowerOff
 );
 
 export default library;
--- src/components/Gulo/SideMenu/SideMenu.js
+++ src/components/Gulo/SideMenu/SideMenu.js
@@ -1,23 +1,46 @@
 import React, {Component} from 'react';
 import './style.scss';
 import {connect}            from 'react-redux';
-import {bindActionCreators} from 'redux';
 import {logout}             from '../../../actions/user';
+import {toggleMenu}         from '../../../actions/UI';
 import MenuToggler          from '../../Misc/MenuToggler';
+import Icon                 from '../../Misc/Icon';
 
 
 class SideMenu extends Component{
   render(){
-    let {isMenu} = this.props;
+    let {isMenu, user} = this.props;
     //if(!isMenu) return null;
 
     return(
       <div className={`SideMenu ${!isMenu && 'hidden'}`}>
-        <div className='overlay'></div>
+        <div className='overlay' onClick={() => this.props.toggleMenu()}></div>
         <main>
-          <div><MenuToggler /></div>
-          <div><button onClick={() => this.props.logout()}>LogOut</button></div>
-          <div>3</div>
+          <div className='toggler'><MenuToggler /></div>
+          {/*<div><button onClick={() => this.props.logout()}>LogOut</button></div>*/}
+          <div className='profile'>
+            <div className='pic'>
+              <img src={user.pic} alt='' />
+            </div>
+            <div>
+              {user.firstname} {user.lastname}
+            </div>
+            <div className='hr' />
+          </div>
+          <menu className={`${!isMenu && 'hidden'}`}>
+            <div className='row'>
+              <div className='icon' style={{marginRight:'+3px'}}><Icon icon='clipboard-list' /></div>
+              <div className='title'>הרשימות שלי</div>
+            </div>
+            <div className='row'>
+              <div className='icon'><Icon faType='far' icon='comment' /></div>
+              <div className='title'>התראות</div>
+            </div>
+            <div className='row' onClick={() => this.props.logout()}>
+              <div className='icon'><Icon icon='power-off' /></div>
+              <div className='title'>התנתקות</div>
+            </div>
+          </menu>
         </main>
       </div>
     );
@@ -26,8 +49,4 @@ class SideMenu extends Component{
 
 const mapStateToProps = ({user, isMenu}) => {return {user, isMenu} };
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({logout}, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SideMenu);
+export default connect(mapStateToProps, {logout, toggleMenu})(SideMenu);
