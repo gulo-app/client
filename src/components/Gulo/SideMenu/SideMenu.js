c70fee9c3580a128ab9c3ca5e108e059fd4bdf0a
flomermer <flom.tomer@gmail.com>
2019-04-07 00:27:53 +0300

    sideMenu style fix

--- src/components/Gulo/SideMenu/SideMenu.js
+++ src/components/Gulo/SideMenu/SideMenu.js
@@ -3,16 +3,22 @@ import './style.scss';
 import {connect}            from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {logout}             from '../../../actions/user';
+import MenuToggler          from '../../Misc/MenuToggler';
 
 
 class SideMenu extends Component{
   render(){
     let {isMenu} = this.props;
-    if(!isMenu) return null;
+    //if(!isMenu) return null;
 
     return(
-      <div className='SideMenu'>
-        <button onClick={() => this.props.logout()}>LogOut</button>
+      <div className={`SideMenu ${!isMenu && 'hidden'}`}>
+        <div className='overlay'></div>
+        <main>
+          <div><MenuToggler /></div>
+          <div><button onClick={() => this.props.logout()}>LogOut</button></div>
+          <div>3</div>
+        </main>
       </div>
     );
   }
