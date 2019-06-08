84d84ffb50d61e5971215bdaf58679230987d985
flomermer <flom.tomer@gmail.com>
2019-06-08 20:12:37 +0300

    fix shareList notifications

--- src/FontAwesomeLib.js
+++ src/FontAwesomeLib.js
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faSearch, faEllipsisV, faPlus, faMinus, faUndo } from '@fortawesome/free-solid-svg-icons';
 import { faHome, faDesktop, faHotel, faArchive, faArrowLeft, faTrash, faShareAlt} from '@fortawesome/free-solid-svg-icons';
-import { faClipboardList, faComment, faPowerOff, faBarcode, faTimes, faBroom} from '@fortawesome/free-solid-svg-icons';
+import { faClipboardList, faComment, faPowerOff, faBarcode, faTimes, faBroom, faHandshake, faUserTimes} from '@fortawesome/free-solid-svg-icons';
 
 
 import { faFile, faComment as farComment, faBell} from '@fortawesome/free-regular-svg-icons';
@@ -11,7 +11,7 @@ library.add(
   faBars, faSearch, faHome, faEllipsisV, faPlus, faMinus, faUndo,
   faDesktop, faHotel, faArchive, faFile, faArrowLeft, faTrash,
   faClipboardList, faComment, farComment, faPowerOff, faBarcode, faBell, faTimes,
-  faShareAlt, faWhatsapp, faBroom
+  faShareAlt, faWhatsapp, faBroom, faHandshake, faUserTimes
 );
 
 export default library;
--- src/components/Gulo/ViewNotification/SharedList/SharedList.js
+++ src/components/Gulo/ViewNotification/SharedList/SharedList.js
@@ -3,6 +3,8 @@ import './style.scss';
 import {connect}              from 'react-redux';
 import {bindActionCreators}   from 'redux';
 import {confirmNotification}  from '../../../../actions/notification';
+import moment from 'moment';
+import Icon from '../../../Misc/Icon';
 
 class SharedList extends Component{
   isConfirmation(){
@@ -16,18 +18,39 @@ class SharedList extends Component{
   }
   render(){
     let {noti} = this.props;
-    if(!noti) return null
+    let {status} = noti;
+    if(!noti) return null;
+    let isToday = moment(noti.createdAt).isSame(moment(), 'date');
+    let icon;
+    if(status===1) //wait for confirmation
+      icon = 'share-alt'
+    else if(status===2 || status===3) //user removed from list
+      icon = 'user-times';
+    else if(status===4 || status===10) //user joined group
+      icon = 'handshake';
     return(
       <div className='SharedList'>
         <main>
-          <div className='primary'>
-            {noti.title.primary}
+          <div className='title'>
+            <div className='topic'>
+                {noti.title.primary}
+                <div className='secondary'>
+                  {noti.title.secondary}
+                </div>
+            </div>
+            <div className='at'>
+              <div className='time'>{moment(noti.createdAt).format('HH:mm')}</div>
+              {!isToday && <div className='date'>{moment(noti.createdAt).format('DD/MM/YY')}</div>}
+            </div>
           </div>
-          <div className='secondary'>
+          <div className='img'>
+            <Icon icon={icon}/>
+          </div>
+          <div className='question'>
             {noti.title.secondary}
           </div>
-          {this.isConfirmation()}
         </main>
+        {this.isConfirmation()}
       </div>
     );
   }
